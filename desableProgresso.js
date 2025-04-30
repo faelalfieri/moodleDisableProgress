392799239219b11148c5d9a459c21b768e03366a
faelalfieri <146894515+faelalfieri@users.noreply.github.com>
2025-04-30 12:35:18 -0300

    Primeiro Commit

---
+++ desableProgresso.js
@@ -0,0 +1,23 @@
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+        // Aplica os estilos dinamicamente para desabilitar links
+        const style = document.createElement("style");
+        style.textContent = `
+            .progressEventInfo a {
+                pointer-events: none;
+            }
+         `;
+        document.head.appendChild(style);
+
+        // Função para desativar o data-haslink
+        const disableHasLink = (selector) => {
+            document.querySelectorAll(selector).forEach(el => {
+                el.setAttribute('data-haslink', 'false');
+            });
+        };
+
+        // Desativa links para os elementos com classes específicas
+        disableHasLink('.progressBarCell.futureNotCompleted');
+        disableHasLink('.progressBarCell.completed');
+    });
+</script>
